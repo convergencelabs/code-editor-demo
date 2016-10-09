bdda09f71d5811cbe637ebb0a66f60c029ccb077
Michael MacFadden <michael@convergencelabs.com>
2016-10-08 21:29:19 -0500

    Updated the status bar.

--- src/jsx/editor/EditorParticipants.jsx
+++ src/jsx/editor/EditorParticipants.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 export default class EditorParticipants extends React.Component {
   render() {
     var participants = this.props.participants.map((participant) => {
-      return (<div key={participant.username} className="participant-indicator" style={{background: participant.color}}></div>);
+      return <div key={participant.username} className="participant-indicator" style={{background: participant.color}}></div>;
     });
 
     return (
       <div className="status-bar-participants">
+        <span>{this.props.participants.length}</span>
         <i className="fa fa-users"/>
         <div className="participant-indicators">{participants}</div>
       </div>
