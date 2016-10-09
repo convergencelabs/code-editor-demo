994ff259ca62bfbd577539bd6384001bd69f3657
Michael MacFadden <michael@convergencelabs.com>
2016-10-08 22:51:36 -0500

    Added a temporary tooltip.

--- src/jsx/editor/EditorParticipants.jsx
+++ src/jsx/editor/EditorParticipants.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class EditorParticipants extends React.Component {
   render() {
     var participants = this.props.participants.map((participant) => {
-      return <div key={participant.username} className="participant-indicator" style={{background: participant.color}}></div>;
+      return <div key={participant.username} title={participant.username} className="participant-indicator" style={{background: participant.color}}></div>;
     });
 
     return (
